0ff5fb4bd30b87eaa31a7a54d74933764a7c9ac7
greatmindsconcept <oyegunwaoluyemi@gmail.com>
2023-01-11 09:18:19 -0800

    fine

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    def __init__(self):
+        pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+baseGeometry class
+"""
+
+
+class BaseGeometry:
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
